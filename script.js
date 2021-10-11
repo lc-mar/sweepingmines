4b14ea758bd5882b9dcf14ccf2fea913aa5bd754
Lauren <78833615+lc-mar@users.noreply.github.com>
2021-10-11 16:23:35 -0400

    initial commit from backup, had to delete old repo

---
+++ script.js
@@ -0,0 +1,13 @@
+// design 
+
+// 1. Populate board with tiles/mines
+// 2. left click on tiles
+    // a. reveal tiles
+// 3. right click on tiles
+    // a. mark tiles
+// 4. check for win/loss
+
+
+import { createBoard} from './minesweeper.js'
+
+console.log(createBoard(2,2))
